4f473389a3294e75315daa19e9af0bfa7cc87613
salartahanian <salartahanian94@gmail.com>
2024-10-06 19:34:16 +0330

    update to shop project 15

--- Page1/shop1.js
+++ Page1/shop1.js
@@ -3,3 +3,11 @@ const navig = document.querySelector('.navig-left2');
 navig.addEventListener('click', function () {
     this.classList.toggle('active');
 });
+
+
+
+const menu2 = document.querySelector('.menu2');
+
+menu2.addEventListener('click', function () {
+    this.classList.toggle('activee');
+});
